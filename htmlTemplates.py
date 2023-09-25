1da94dd6d8a85398036aaf02434d496b56860a49
Xiaoguo Liu <liu_xiao_guo@yahoo.com>
2023-09-25 15:57:09 +0800

    Added new files and made modifications

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -31,6 +31,7 @@ bot_template = '''
         <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
+    <div class="message">{{MSG1}}</div>
 </div>
 '''
 
@@ -40,5 +41,6 @@ user_template = '''
         <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
     </div>    
     <div class="message">{{MSG}}</div>
+    <div class="message">{{MSG1}}</div>
 </div>
 '''
